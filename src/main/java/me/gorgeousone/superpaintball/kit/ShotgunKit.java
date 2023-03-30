5a003b0f2174bfff3f2b4d3746fab996ee865f83
IEatSushi <2klaus.4peter@gmail.com>
2023-03-30 02:33:20 +0200

    tune guns again

--- src/main/java/me/gorgeousone/superpaintball/kit/ShotgunKit.java
+++ src/main/java/me/gorgeousone/superpaintball/kit/ShotgunKit.java
@@ -14,7 +14,7 @@ public class ShotgunKit extends AbstractKit {
 	private final JavaPlugin plugin;
 	
 	public ShotgunKit(JavaPlugin plugin) {
-		super(KitType.SHOTGUN, 1, 8, 1f, .3f, 30, Sound.ENTITY_CHICKEN_EGG, 1f, .85f);
+		super(KitType.SHOTGUN, 1, 8, 1.25f, .35f, 30, Sound.ENTITY_CHICKEN_EGG, 1f, .85f);
 		this.plugin = plugin;
 	}
 	
