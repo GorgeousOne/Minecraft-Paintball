247c4e7c0499b7bfc888d9ceff97d65721f85caf
IEatSushi <2klaus.4peter@gmail.com>
2023-04-20 01:59:53 +0200

    add kit descriptions

--- src/main/java/me/gorgeousone/paintball/kit/KitType.java
+++ src/main/java/me/gorgeousone/paintball/kit/KitType.java
@@ -6,25 +6,36 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Arrays;
+
 public enum KitType {
 	
-	RIFLE(ChatColor.YELLOW + "Paintball Rifle"),
-	SHOTGUN(ChatColor.YELLOW + "Paintball Shotgun"),
-	MACHINE_GUN(ChatColor.YELLOW + "Paintball Machine Gun"),
-//	SNIPER(ChatColor.YELLOW + "Paintball Sniper")
+	RIFLE("Paintball Rifle", "Semi-automatic paintball rifle.", "High range and accuracy."),
+	SHOTGUN("Paintball Shotgun", "Pump action paintball shotgun.", "Shoots a cloud of bullets with", "low range and accuracy."),
+	MACHINE_GUN("Paintball Machine Gun", "Full-automatic paintball gun.", "Accuracy drops with longer use."),
+//	SNIPER(ChatColor.YELLOW + "Paintball Sniper", "Long range sniper rifle.", "Higher damage the longer scoped (sneak).")
 	;
 	
 	public final String gunName;
+	public final String[] gunLore;
 	public Material gunMaterial;
 	private ItemStack gunItem;
 	
-	KitType(String displayName) {this.gunName = displayName;}
+	KitType(String displayName, String... lore) {
+		this.gunName = ChatColor.YELLOW + displayName;
+		this.gunLore = lore;
+		
+		for (int i = 0; i < lore.length; ++i) {
+			lore[i] = ChatColor.GRAY + lore[i];
+		}
+	}
 	
 	private void setGunMaterial(Material gunMaterial) {
 		this.gunMaterial = gunMaterial;
 		this.gunItem = new ItemStack(gunMaterial);
 		ItemMeta meta = gunItem.getItemMeta();
 		meta.setDisplayName(gunName);
+		meta.setLore(Arrays.asList(gunLore));
 		gunItem.setItemMeta(meta);
 	}
 	
